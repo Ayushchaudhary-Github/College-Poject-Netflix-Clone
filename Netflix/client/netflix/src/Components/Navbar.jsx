74d2dc0b8293d35885c40e88791a131051cf766e
Ayushchaudhary-Github <chaudharyayush467@gmail.com>
2022-11-18 01:15:57 +0530

    ok

--- Netflix/client/netflix/src/Components/Navbar.jsx
+++ Netflix/client/netflix/src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         
         <div className='Container'>
             <div className='left'>
-                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.pJUs7cTaLMMANyY7FI4sSAHaEK%26pid%3DApi&f=1&ipt=914fd3152b2396a460a9c88ba73bd75a2c333c0dffcfeadbc57e1f30682349b9&ipo=images" alt=''/>
+                  <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.pJUs7cTaLMMANyY7FI4sSAHaEK%26pid%3DApi&f=1&ipt=914fd3152b2396a460a9c88ba73bd75a2c333c0dffcfeadbc57e1f30682349b9&ipo=images" alt=" " />
                 <span>Homepage</span>
                 <span>Series</span>
                 <span>Movies</span>
