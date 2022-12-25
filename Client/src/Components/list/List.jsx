ff60819fcf57f8ee4e0bb65ec6a34e103855ad5d
Ayushchaudhary-Github <chaudharyayush467@gmail.com>
2022-12-26 01:05:33 +0530

    list done

--- Client/src/Components/list/List.jsx
+++ Client/src/Components/list/List.jsx
@@ -1,29 +1,58 @@
-import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons'
-import ListItem from '../listitem/ListItem'
-import "./list.scss"
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
+import { useRef, useState } from "react";
+import ListItem from "../listItem/ListItem";
+import "./list.scss";
 
-export default function List() {
+export default function List({ list }) {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 10 - clickLimit) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
   return (
-    <div className='list'>
-      <span className='listTitle'>Contenue to watch</span>
-      <div className='wrapper'>
-        <ArrowBackIosOutlined className='sliderArrow left'/>
-        <div className='Container'>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+
         </div>
-        <ArrowForwardIosOutlined className='sliderArrow right'/>
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
 }
-
--- Client/src/Components/listitem/ListItem.jsx
+++ Client/src/Components/listitem/ListItem.jsx
@@ -1,9 +1,37 @@
+import {
+    PlayArrow,
+    Add,
+    ThumbUpAltOutlined,
+    ThumbDownOutlined,
+} from "@material-ui/icons";
 import "./listitems.scss"
 
 export default function ListItem() {
     return (
         <div className='listItem'>
-            item
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqpvH51WdkjZDNLJhPQKF7jvAXP-9FrKwkLw&usqp=CAU" alt="" />
+
+            <div className="itemInfo">
+                <div className="icons">
+                    <PlayArrow />
+                    <Add />
+                    <ThumbUpAltOutlined />
+                    <ThumbDownOutlined />
+                </div>
+                <div className="itemInfoTop">
+                    <span>1 hour 14 min</span>
+                    <span className="limit">+16</span>
+                    <span>1999</span>
+                </div>
+                <div className="desc">
+                Disgruntled Korean War veteran Walt Kowalski
+                sets out to reform his neighbor,Thao Lor, 
+                a Hmong teenager who tried to steal 
+                Kowalski's prized possession: a 1972
+                 Gran Torino.
+                </div>
+                <div className="genre">Action</div>
+            </div>
         </div>
     )
 }
